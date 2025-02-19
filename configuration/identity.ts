552057ffa5713410f642a48250ae8f28e06b1e3a
aaronwan2 <aaronwan2@gmail.com>
2025-02-19 13:22:00 -0500

    Update identity.ts
    changed ai name

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `NC Vintage Bazaar`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `Bazaar Bot`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
