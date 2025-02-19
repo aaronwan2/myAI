750c3aca7e73777138579985d4f9568d026c1dcf
aaronwan2 <aaronwan2@gmail.com>
2025-02-19 12:44:06 -0500

    Update ui.ts
    changed name and clear button text

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,9 +1,9 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `How, Bazaar??`;
+export const CHAT_HEADER: string = `Bazaar Bot`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Copyright 2025 NC Vintage Bazaar`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
+export const CLEAR_BUTTON_TEXT: string = `Clear Chat`;
 export const PAGE_TITLE: string = `NCVB Chat`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
